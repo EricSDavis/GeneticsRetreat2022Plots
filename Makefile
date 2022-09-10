b7402143c67e6647520bff5891af3cc8aaf90b89
Eric Davis <ericscottdavis@outlook.com>
2022-09-10 13:33:20 -0400

    Update hicMapExample

--- Makefile
+++ Makefile
@@ -1,26 +1,22 @@
 .PHONY: clean
 
 objects :=\
-	data/greeting.rds\
-	plots/example.pdf\
-	tables/example.txt
+	plots/hicMapExample01.pdf\
+	plots/hicMapExample02.pdf\
+	plots/hicMapExample03.pdf\
+	plots/hicMapExample04.pdf\
+	plots/hicMapExample05.pdf
 
 all: $(objects)
 
 clean:
 	rm -rf $(objects)
 
-data/greeting.rds:\
-	scripts/utils/sayHello.R\
-	scripts/processing/makeGreeting.R
-		mkdir -p data
-		Rscript scripts/processing/makeGreeting.R
-
-plots/example.pdf\
-tables/example.txt:\
-	scripts/utils/sayHello.R\
-	data/greeting.rds\
-	scripts/analysis/example.R
-		mkdir -p plots tables
-		Rscript scripts/analysis/example.R
-		
+plots/hicMapExample01.pdf\
+plots/hicMapExample02.pdf\
+plots/hicMapExample03.pdf\
+plots/hicMapExample04.pdf\
+plots/hicMapExample05.pdf:\
+	data/raw/hic/MEGA_K562_WT_4320_inter.hic\
+	scripts/hicMapExample.R
+		Rscript scripts/hicMapExample.R
--- scripts/hicMapExample.R
+++ scripts/hicMapExample.R
@@ -22,8 +22,13 @@ initialHic <- function(p = p) {
   return(hic)
 }
 
+## Define loop
+loop <- 
+  GInteractions(GRanges("7:54750000-54825000"),
+                GRanges("7:55225000-55300000"))
+
 ## Define function for plotting region
-plotRegion <- function(p = p, hic = hic, highlight=FALSE) {
+plotRegion <- function(p = p, hic = hic) {
   
   ## Hi-C plots
   hic2 <- 
@@ -54,34 +59,47 @@ plotRegion <- function(p = p, hic = hic, highlight=FALSE) {
                   just = c("right", "top"),
                   scale = "Mb")
   
-  ## Define loop
-  loop <- 
-    GInteractions(GRanges("7:54750000-54825000"),
-                  GRanges("7:55225000-55300000"))
+}
+
+addLoop <- function(hic=hic, loop=loop) {
   
   ## Annotate loop
   annoPixels(plot = hic,
              data = loop,
              shift = 0)
   
-  ## Highlight loop
-  if (highlight) {
-    annoHighlight(plot = hic,
-                  params = p,
-                  chrom = as.character(seqnames(anchors(loop, 'first'))),
-                  chromstart = start(anchors(loop, 'first')),
-                  chromend = end(anchors(loop, 'first')))
-    annoHighlight(plot = hic,
-                  params = p,
-                  chrom = as.character(seqnames(anchors(loop, 'second'))),
-                  chromstart = start(anchors(loop, 'second')),
-                  chromend = end(anchors(loop, 'second')))
-  }
+}
+
+addLoopHighlight <- function(p=p, hic=hic, loop=loop) {
+  
+  annoHighlight(plot = hic,
+                params = p,
+                chrom = as.character(seqnames(anchors(loop, 'first'))),
+                chromstart = start(anchors(loop, 'first')),
+                chromend = end(anchors(loop, 'first')))
+  annoHighlight(plot = hic,
+                params = p,
+                chrom = as.character(seqnames(anchors(loop, 'second'))),
+                chromstart = start(anchors(loop, 'second')),
+                chromend = end(anchors(loop, 'second')))
   
 }
 
+addOffDiagonal <- function(p=p, hic=hic, loop=loop) {
+  plotHicSquare(data = "data/raw/hic/MEGA_K562_WT_4320_inter.hic",
+                params = p,
+                chrom = as.character(seqnames(anchors(loop, 'first'))),
+                chromstart = start(anchors(loop, 'first')),
+                chromend = end(anchors(loop, 'first')),
+                altchrom = as.character(seqnames(anchors(loop, 'second'))),
+                altchromstart = start(anchors(loop, 'second')),
+                altchromend = end(anchors(loop, 'second')),
+                half = "bottom")
+}
+
+
 ## Initialize page
-pageCreate(width = 4, height = 4, showGuides = FALSE)
+pageCreate(width = 8, height = 4, showGuides = FALSE)
 
 ## Define common params
 p <- pgParams(assembly = "hg38",
@@ -110,18 +128,104 @@ p <- pgParams(assembly = "hg38",
 hic <- initialHic(p = p)
 
 ## Plot region
-pageCreate(width = 4, height = 4, showGuides = FALSE)
-plotRegion(p = p, hic = hic, highlight = TRUE)
-
-## 10Kb resolution
-pageCreate(width = 4, height = 4, showGuides = FALSE)
-p$resolution <- 10e03
-p$zrange <- c(0, 300)
+pdf(file = "plots/hicMapExample01.pdf", width = 8, height = 4)
+pageCreate(width = 8, height = 4, showGuides = FALSE)
 plotRegion(p = p, hic = hic)
+dev.off()
 
-
-## 5Kb resolution
-pageCreate(width = 4, height = 4, showGuides = FALSE)
-p$resolution <- 5e03
-p$zrange <- c(0, 100)
+## Show genomic loci and loop
+pdf(file = "plots/hicMapExample02.pdf", width = 8, height = 4)
+pageCreate(width = 8, height = 4, showGuides = FALSE)
 plotRegion(p = p, hic = hic)
+addLoop(hic = hic, loop = loop)
+addLoopHighlight(p = p, hic = hic, loop = loop)
+dev.off()
+
+## Show off diagonal zoom of loop
+p2 <- p
+p2$x <- 4.0
+p2$y <- 1.0
+p2$width <- 1.0
+p2$height <- 1.0
+pdf(file = "plots/hicMapExample03.pdf", width = 8, height = 4)
+pageCreate(width = 8, height = 4, showGuides = FALSE)
+plotRegion(p = p, hic = hic)
+addLoop(hic = hic, loop = loop)
+addOffDiagonal(p = p2, hic = hic, loop = loop)
+plotText(label = "25-Kb Pixels",
+         x = p2$x,
+         y = p2$y - p2$space/4,
+         just = c('left', "bottom"))
+dev.off()
+
+## Finer resolution (10Kb)
+pdf(file = "plots/hicMapExample04.pdf", width = 8, height = 4)
+pageCreate(width = 8, height = 4, showGuides = FALSE)
+p3 <- p
+p3$resolution <- 10e03
+p3$zrange <- c(0, 300)
+plotRegion(p = p3, hic = hic)
+addLoop(hic = hic, loop = loop)
+
+## Show off diagonal zoom of loop
+p2$x <- 4.0
+p2$y <- 1.0
+p2$width <- 1.0
+p2$height <- 1.0
+addOffDiagonal(p = p2, hic = hic, loop = loop)
+plotText(label = "25-Kb Pixels",
+         x = p2$x,
+         y = p2$y - p2$space/4,
+         just = c('left', "bottom"))
+
+p3$x <- 5.25
+p3$y <- 1.0
+p3$width <- 1.0
+p3$height <- 1.0
+addOffDiagonal(p = p3, hic = hic, loop = loop)
+plotText(label = "10-Kb Pixels",
+         x = p3$x,
+         y = p3$y - p3$space/4,
+         just = c('left', "bottom"))
+dev.off()
+
+## Finer resolution (5Kb)
+pdf(file = "plots/hicMapExample05.pdf", width = 8, height = 4)
+pageCreate(width = 8, height = 4, showGuides = FALSE)
+p4 <- p
+p4$resolution <- 5e03
+p4$zrange <- c(0, 100)
+plotRegion(p = p4, hic = hic)
+addLoop(hic = hic, loop = loop)
+
+## Show off diagonal zoom of loop
+p2$x <- 4.0
+p2$y <- 1.0
+p2$width <- 1.0
+p2$height <- 1.0
+addOffDiagonal(p = p2, hic = hic, loop = loop)
+plotText(label = "25-Kb Pixels",
+         x = p2$x,
+         y = p2$y - p2$space/4,
+         just = c('left', "bottom"))
+
+p3$x <- 5.25
+p3$y <- 1.0
+p3$width <- 1.0
+p3$height <- 1.0
+addOffDiagonal(p = p3, hic = hic, loop = loop)
+plotText(label = "10-Kb Pixels",
+         x = p3$x,
+         y = p3$y - p3$space/4,
+         just = c('left', "bottom"))
+
+p4$x <- 6.5
+p4$y <- 1.0
+p4$width <- 1.0
+p4$height <- 1.0
+addOffDiagonal(p = p4, hic = hic, loop = loop)
+plotText(label = "5-Kb Pixels",
+         x = p4$x,
+         y = p4$y - p4$space/4,
+         just = c('left', "bottom"))
+dev.off()
